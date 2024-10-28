7d972a75370d76e31cc9acfad7e45aa9f65f6f28
Aplikasimedia <syaifulabdad@gmail.com>
2024-10-28 09:45:27 +0700

    Update dapo-javascript.js

--- dapo-javascript.js
+++ dapo-javascript.js
@@ -1,35 +1,33 @@
-<script>
-  console.log('testing.......');
-  class ApiClient {
-    constructor(baseURL) {
-      this.baseURL = baseURL;
-      this.token = null;
-    }
-  
-    setToken(token) {
-      this.token = token;
-    }
-  
-    async request(endpoint, options = {}) {
-      const headers = {
-        ...options.headers,
-        'Authorization': `Bearer ${this.token}`,
-      };
-  
-      const response = await fetch(`${this.baseURL}${endpoint}`, {
-        ...options,
-        headers,
-      });
-  
-      return response.json();
-    }
+console.log('testing.......');
+class ApiClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+    this.token = null;
   }
 
-  const client = new ApiClient('https://api.example.com');
-  client.setToken('your-bearer-token');
-  
-  // Example API request
-  client.request('/data', {
-    method: 'GET',
-  }).then(data => console.log(data));
-</script>
+  setToken(token) {
+    this.token = token;
+  }
+
+  async request(endpoint, options = {}) {
+    const headers = {
+      ...options.headers,
+      'Authorization': `Bearer ${this.token}`,
+    };
+
+    const response = await fetch(`${this.baseURL}${endpoint}`, {
+      ...options,
+      headers,
+    });
+
+    return response.json();
+  }
+}
+
+const client = new ApiClient('https://api.example.com');
+client.setToken('your-bearer-token');
+
+// Example API request
+client.request('/data', {
+  method: 'GET',
+}).then(data => console.log(data));
